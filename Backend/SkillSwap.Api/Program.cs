125a94bbfd8a801205c15b6a50e907a1257396f5
timabloom <timabloom@proton.me>
2024-09-09 18:11:29 +0200

    feat: allow frontend origins

--- Backend/SkillSwap.Api/Program.cs
+++ Backend/SkillSwap.Api/Program.cs
@@ -5,6 +5,19 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<SkillSwapContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.")));
 
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      policy =>
+                      {
+                          policy.WithOrigins("http://localhost:5173")
+                              .WithHeaders("Content-Type")
+                              .WithMethods("GET", "POST", "PUT", "DELETE");
+                      });
+});
+
 builder.Services.AddControllers();
 
 builder.Services.AddEndpointsApiExplorer();
@@ -17,6 +30,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors(MyAllowSpecificOrigins);
 
 app.UseHttpsRedirection();
 
