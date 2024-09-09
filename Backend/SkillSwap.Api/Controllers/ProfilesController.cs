3aa74b08308b3016d192a6220231c13a62a57495
timabloom <timabloom@proton.me>
2024-09-09 17:01:55 +0200

    feat: implement post profile endpoint

--- Backend/SkillSwap.Api/Controllers/ProfilesController.cs
+++ Backend/SkillSwap.Api/Controllers/ProfilesController.cs
@@ -1,4 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
+using SkillSwap.Api.Dtos;
+using SkillSwap.Api.Models;
 
 namespace SkillSwap.Api.Controllers;
 
@@ -7,4 +9,26 @@ namespace SkillSwap.Api.Controllers;
 public class ProfilesController(SkillSwapContext context) : ControllerBase
 {
     private readonly SkillSwapContext _context = context;
+    
+    [HttpPost]
+    public async Task<ActionResult> CreateProfile(ProfileCreateRequest requestBody)
+    {
+        var profile = new Profile
+        {
+            PublicId = Guid.NewGuid(),
+            Name = requestBody.Name,
+            Bio = requestBody?.Bio,
+            ImageUrl = requestBody?.ImageUrl,
+            Skills = requestBody?.Skills?.Select(x => new Skill { TagName = x.TagName }).ToList() ?? [],
+            Needs = requestBody?.Needs?.Select(x => new Need { TagName = x.TagName }).ToList() ?? [],
+            ContactInformation = new ContactInformation
+            {
+                Email = requestBody?.ContactInformation?.Email ?? ""
+            }
+        };
+        await _context.Profiles.AddAsync(profile);
+        await _context.SaveChangesAsync();
+
+        return CreatedAtAction("GetProfile", new { profile.PublicId }, (ProfileGetResponse)profile);
+    }
 }
