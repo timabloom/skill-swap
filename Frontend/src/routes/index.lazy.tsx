30ad8b4427d991f27d0b88b04ca9ee4f14b34c58
timabloom <timabloom@proton.me>
2024-09-10 10:01:19 +0200

    feat: add styling to hero section

--- Frontend/src/routes/index.lazy.tsx
+++ Frontend/src/routes/index.lazy.tsx
@@ -1,3 +1,4 @@
+import { SignedOut, SignUpButton } from '@clerk/clerk-react'
 import { createLazyFileRoute } from '@tanstack/react-router'
 
 export const Route = createLazyFileRoute('/')({
@@ -7,10 +8,18 @@ export const Route = createLazyFileRoute('/')({
 function Index() {
 
     return (
-        <>
-        <h1>Trade Skills, Build Connections!</h1>
-        <p>Sign up to get started</p>
-        <img src="" alt="photo of two people collaborating" />
-        </>
+        <div className="flex p-2">
+            <div className="p-10">
+                <h1 className='mt-36 text-8xl'>Trade Skills, Build Connections!</h1>
+                <div className="flex flex-col">
+                    <p className="text-3xl pt-10">Register now to start collaborating and building valuable connections.</p>
+                    <SignedOut>
+                        <SignUpButton className="btn btn-primary w-36 text-xl mt-4">Sign Up</SignUpButton>
+                    </SignedOut>
+                </div>
+            </div>
+            <img className="p-10 pl-0 max-w-4xl rounded-" style={{ borderRadius: "25% 75% 71% 29% / 65% 69% 31% 35%" }} src="/pexels-buro-millennial-636760-1438084.avif
+            " />
+        </div>
     )
 }
