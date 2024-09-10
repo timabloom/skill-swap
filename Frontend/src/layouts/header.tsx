5313db7224110780bb34aa9be8035bfd33221867
timabloom <timabloom@proton.me>
2024-09-10 14:15:34 +0200

    refactor: seperate header from root page

---
+++ Frontend/src/layouts/header.tsx
@@ -0,0 +1,42 @@
+import { SignedIn, SignedOut, SignInButton, SignOutButton, SignUpButton } from '@clerk/clerk-react'
+import { Link } from '@tanstack/react-router'
+
+function Header() {
+
+    return (
+        <header className="flex items-center justify-between p-4 pl-6 pr-6">
+            <Link to="/" className="[&.active]:font-bold flex gap-1">
+                <p className="font-bold text-4xl">Skill</p>
+                <p className="font-bold text-4xl text-yellow-500">Swap</p>
+            </Link>
+            <div className="flex items-center gap-4">
+                <Link to="/" className="[&.active]:font-bold text-xl">
+                    Home
+                </Link>
+                <Link to="/profile" className="[&.active]:font-bold text-xl">
+                    Profile
+                </Link>
+                <Link to="/matches" className="[&.active]:font-bold text-xl">
+                    Matches
+                </Link>
+                <Link to="/connections" className="[&.active]:font-bold text-xl">
+                    Connections
+                </Link>
+                <Link to="/about" className="[&.active]:font-bold text-xl">
+                    About
+                </Link>
+                <SignedOut>
+                    <SignInButton className="text-xl" forceRedirectUrl="/profile" />
+                </SignedOut>
+                <SignedIn >
+                    <SignOutButton className="text-xl" />
+                </SignedIn>
+                <SignedOut>
+                    <SignUpButton className="btn btn-primary w-32 text-xl" signInForceRedirectUrl="/create-profile" >Sign Up</SignUpButton>
+                </SignedOut>
+            </div>
+        </header>
+    )
+}
+
+export default Header
