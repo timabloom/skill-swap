d7dc7a7211317084a12ba25c1410672606051f2d
timabloom <timabloom@proton.me>
2024-09-10 10:44:19 +0200

    feat: add styling to create a profile form

--- Frontend/src/routes/profile.lazy.tsx
+++ Frontend/src/routes/profile.lazy.tsx
@@ -25,16 +25,28 @@ function Profile() {
   const onSubmit: SubmitHandler<Inputs> = (data) => postProfile({ ...data, clerkId: user?.id }, data.picture)
 
   return (
-    <form className="flex flex-col" onSubmit={handleSubmit(onSubmit)}>
-      <input className="file-input file-input-bordered file-input-primary w-full" type="file" {...register("picture")} />
-      <label htmlFor="name">Name* {errors.name && <span>This field is required</span>}</label>
-      <input className="input input-bordered input-primary w-full" {...register("name", { required: true })} />
-      <label htmlFor="bio">Bio</label>
-      <input className="input input-bordered input-primary w-full" {...register("bio", { required: false })} />
-      <p>Contact Information</p>
-      <label htmlFor="email">Email</label>
-      <input className="input input-bordered input-primary w-full" {...register("email", { required: false })} />
-      <button className="btn btn-primary" type="submit">Submit</button>
-    </form>
+    <div className="flex items-center justify-center p-10">
+      <form className="flex flex-col max-w-96 border rounded-md p-12" onSubmit={handleSubmit(onSubmit)}>
+        <h1 className="text-3xl text-center pb-8">Create Profile</h1>
+
+        <div className="border rounded-2xl w-72 h-72 flex flex-col justify-end items-center">
+          <div className="w-24 h-24 bg-gray-300 rounded-full mb-2" rounded-full ></div>
+          <div className="w-24 h-32 bg-gray-300 rounded-t-full" rounded-full ></div>
+        </div>
+
+        <input className="file-input file-input-bordered file-input-primary w-full mt-4" type="file" {...register("picture")} />
+
+        <label className="pt-8 pb-1" htmlFor="name">Name* {errors.name && <span>This field is required</span>}</label>
+        <input className="input input-bordered input-primary w-full" {...register("name", { required: true })} />
+
+        <label className="pt-8 pb-1" htmlFor="bio">Bio</label>
+        <textarea className="textarea textarea-primary min-h-40" {...register("bio", { required: false })} />
+
+        <h2 className="text-xl pt-10 pb-4">Contact Information</h2>
+        <label className="pb-1" htmlFor="email">Email</label>
+        <input className="input input-bordered input-primary w-full" {...register("email", { required: false })} />
+        <button className="btn btn-primary mt-8" type="submit">Submit</button>
+      </form>
+    </div>
   )
 }
